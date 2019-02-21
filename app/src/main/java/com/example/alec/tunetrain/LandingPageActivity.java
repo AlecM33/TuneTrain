688ee749863c63fbc5c75545bf45c62553fb01d3
ericcparsons <ecparsons42@gmail.com>
2019-02-21 15:46:43 -0500

    added lifecycle methods and logs for landing page and the new stats activity, as well as made the stats button functional

--- app/src/main/java/com/example/alec/tunetrain/LandingPageActivity.java
+++ app/src/main/java/com/example/alec/tunetrain/LandingPageActivity.java
@@ -1,7 +1,11 @@
 package com.example.alec.tunetrain;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LandingPageActivity extends AppCompatActivity {
@@ -11,14 +15,52 @@ public class LandingPageActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
 
+    private static final String TAG = "LandingPageActivity";
+    private Button mStatsButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.landing_page);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        //event listener for statsButton
+        mStatsButton = (Button)findViewById(R.id.stats_button);
+        mStatsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start StatsActivity
+                Intent intent = new Intent(LandingPageActivity.this, StatsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 
     /**
---
+++ app/src/main/java/com/example/alec/tunetrain/StatsActivity.java
@@ -0,0 +1,46 @@
+package com.example.alec.tunetrain;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class StatsActivity extends AppCompatActivity {
+
+    private static final String TAG = "StatsActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
+
+        setContentView(R.layout.activity_stats);
+
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+}
