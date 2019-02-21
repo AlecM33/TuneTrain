65a1f00e897a580d83e427ae69cbfd0f6962c5f8
Alec Maier <Maier.144@osu.edu>
2019-02-21 03:12:47 -0500

    Basic layout and activity for landing page

--- app/src/main/java/com/example/alec/tunetrain/LandingPageActivity.java
+++ app/src/main/java/com/example/alec/tunetrain/LandingPageActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class LandingPageActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.landing_page);
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
