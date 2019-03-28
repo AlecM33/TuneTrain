c465e61d7c46e39b2a6b2f5ab9fbd1a4f9eadbb3
Alec Maier <Maier.144@osu.edu>
2019-03-28 11:52:53 -0400

    Added todos

--- app/src/main/java/com/example/alec/tunetrain/StatsActivity.java
+++ app/src/main/java/com/example/alec/tunetrain/StatsActivity.java
@@ -32,6 +32,7 @@ public class StatsActivity extends AppCompatActivity {
         db = AppDatabase.getAppDatabase(this.getBaseContext());
         StatsActivity.GetStatTask statTask = new StatsActivity.GetStatTask();
         mCorrectPercentage = (TextView)findViewById(R.id.percent_correct);
+        // TODO: Refactor
         try {
             allGuesses = statTask.execute().get();
         } catch (ExecutionException e) {
@@ -47,6 +48,7 @@ public class StatsActivity extends AppCompatActivity {
         }
     }
 
+    // TODO: Refactor
     private class GetStatTask extends AsyncTask<Void, Void, List<Guess>> {
         Template currentTemplate;
 
